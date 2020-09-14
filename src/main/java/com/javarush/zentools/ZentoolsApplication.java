1337247c4201daea0cfc802bed8f9841b6f95002
anzor <anzor.karmov@loyaltyplant.com>
2020-09-14 21:15:37 +0300

    first commit

---
+++ src/main/java/com/javarush/zentools/ZentoolsApplication.java
@@ -0,0 +1,13 @@
+package com.javarush.zentools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZentoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javarush/zentools/ZentoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarush.zentools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
