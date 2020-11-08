fe56fd53613322bafcc84091ac22537f4663e4ca
anzor <anzor.karmov@loyaltyplant.com>
2020-11-08 16:50:20 +0300

    fix naming

--- src/main/java/com/javarush/myactivities/repositories/interfaces/CrudRepository.java
+++ src/main/java/com/javarush/myactivities/repositories/interfaces/CrudRepository.java
@@ -1,7 +1,5 @@
 package com.javarush.myactivities.repositories.interfaces;
 
-import com.javarush.myactivities.entities.Project;
-
 import java.util.List;
 
 public interface CrudRepository <T, R> {
@@ -9,7 +7,7 @@ public interface CrudRepository <T, R> {
     List<T> getAll();
     T getById(R id);
     void delete(R id);
-    R create(T project);
-    void update(T project);
+    R create(T entity);
+    void update(T entity);
 
 }
