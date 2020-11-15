0c5e512e23e073646ad8a2a030703884f2a800ce
anzor <anzor.karmov@gmail.com>
2020-11-15 19:50:20 +0300

    activity service
    (cherry picked from commit d2563390a9f23b02b123e1c667a778b2b290433a)

---
+++ src/main/java/com/javarush/myactivities/services/interfaces/ActivityService.java
@@ -0,0 +1,6 @@
+package com.javarush.myactivities.services.interfaces;
+
+import com.javarush.myactivities.entities.Activity;
+
+public interface ActivityService extends CrudService<Activity, Long> {
+}
