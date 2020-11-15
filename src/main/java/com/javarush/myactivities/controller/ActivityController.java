55b6d0f9233c742dbfa4f0c067d5c6b044678bc1
anzor <anzor.karmov@gmail.com>
2020-11-15 19:57:10 +0300

    activities list ui

--- src/main/java/com/javarush/myactivities/controller/ActivityController.java
+++ src/main/java/com/javarush/myactivities/controller/ActivityController.java
@@ -1,14 +1,25 @@
 package com.javarush.myactivities.controller;
 
+import com.javarush.myactivities.services.interfaces.ActivityService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/activities")
 public class ActivityController {
 
+    private final ActivityService activityService;
+
+    @Autowired
+    public ActivityController(ActivityService activityService) {
+        this.activityService = activityService;
+    }
+
     @RequestMapping("/")
-    public String getActivities() {
+    public String getActivities(Model model) {
+        model.addAttribute("activities", activityService.getAll());
         return "activities/list";
     }
 
